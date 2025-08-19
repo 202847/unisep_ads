62abf1d0a0f23e534568149615727d20026e74eb
202847 <bruno.steinick@unisep.edu.br>
2025-08-19 19:33:31 -0300

    Primeiro commit com arquivo js

---
+++ primeiro_proejto_1/index.js
@@ -0,0 +1,37 @@
+var nome = "Bruno";
+var salario =150.0;
+var status = true;
+
+
+const pessoas = [];
+
+pessoas.push("Bruno");
+pessoas.push("Gabriel");
+
+const aluno = {
+    nome : "Bruno",
+    RA : "123456",
+    email: "steinickbruno@gmail.com"
+
+}
+
+aluno.telefone = "46 999202847"
+
+console.log('nome: ',nome)
+console.log("salario:" , salario);
+console.log("situação:", status);
+console.log("pessoas:", pessoas);
+console.log("total de pessoas:", pessoas.length);
+console.log("aluno:", aluno);
+
+
+for(var i = 0; i < pessoas.length ; i++){
+    console.log(pessoas[i]);
+}
+
+pessoas.forEach(i=>{
+    console.log(i)
+});
+
+
+
